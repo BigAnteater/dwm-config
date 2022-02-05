d61f2088342b0fb5945b38c3035210374ba021fd
BigAnteater <77298458+BigAnteater@users.noreply.github.com>
2022-02-04 17:25:15 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ mv dmenu ..
 mv slstatus ..
 mv st ..
 cd ../dwm && make clean install && cd ..
-cd ../dmenu && make clean install && cd ..
-cd ../slstatus && make clean install && cd ..
-cd ../st && make clean install && cd ..
+cd dmenu && make clean install && cd ..
+cd slstatus && make clean install && cd ..
+cd st && make clean install && cd ..
 
 mv dwm.desktop /usr/share/xsessions/
 pacman -S nitrogen
