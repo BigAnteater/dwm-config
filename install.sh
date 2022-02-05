38588b00cf1348bf5e277974992743501addbbde
BigAnteater <77298458+BigAnteater@users.noreply.github.com>
2022-02-04 17:21:13 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 echo "This will set up DWM."
 sleep 2s
+mv dwm ..
+mv dmenu ..
+mv slstatus ..
+mv st ..
 cd dwm && make clean install && cd ..
 cd dmenu && make clean install && cd ..
 cd slstatus && make clean install && cd ..
