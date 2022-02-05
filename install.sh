3863f89deb4de8330331989d197f089356bf3d6b
BigAnteater <77298458+BigAnteater@users.noreply.github.com>
2022-02-04 17:08:24 -0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "This will set up DWM."
+sleep 2s
+cd dwm && make clean install && cd ..
+cd dmenu && make clean install && cd ..
+cd slstatus && make clean install && cd ..
+cd st && make clean install && cd ..
+
+mv dwm.desktop /usr/share/xsessions/
+pacman -S nitrogen
+
+ehco "exec dwm &
+nitrogen --restore &
+slstatus &" > .profile
+
+echo "DWM successfully installed!"
